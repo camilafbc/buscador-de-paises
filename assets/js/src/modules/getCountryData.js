4231a05f85ce128d03969c9d41bab2c3dbd4da8c
camilafbc <camilafernanda914@gmail.com>
2023-04-18 17:47:54 -0300

    icons

--- assets/js/src/modules/getCountryData.js
+++ assets/js/src/modules/getCountryData.js
@@ -2,13 +2,28 @@ const container = document.getElementById("card-container")
 const numberFormat = new Intl.NumberFormat('pt-BR')
 
 async function buscador(entrada){
+
     const busca = await fetch(`https://restcountries.com/v3.1/translation/${entrada}`)
-        await busca.json()
-            .then((response) => response.forEach((pais) => container.innerHTML += createCard(pais)))
+    const response = await busca.json()
+        response.forEach((pais) => container.innerHTML += createCard(pais))
+            
 }
+
 
 function clearContainer(){
     container.innerHTML = ""
+}
+
+function error(erro){
+    return `
+        <div class="text-center mt-3 mb-3 error">
+            <img src="assets/img/alert-icon.svg" >
+            <p>${erro}</p>
+            <p class="mt-3">
+                A pesquisa não retornou resultados. Verifique as informações inseridas e tente novamente
+            </p>
+        </div>
+    `
 }
 
 function createCard(country){
@@ -18,7 +33,7 @@ function createCard(country){
         <img src="${country.flags.svg}" class="card-img-top" alt="Bandeira do ${country.translations.por.common}">
         <div class="card-body">
         <h4 class="card-title text-center">${country.translations.por.common}</h4>
-        <h6 class="text-center">Nome oficial: ${country.translations.por.official}</h6>
+        <p class="text-center text-muted fs-6 fw-medium">Nome oficial: ${country.translations.por.official}</p>
         
             <ul class="card-text text-start">
                 <li> 
@@ -38,7 +53,7 @@ function createCard(country){
                     <span class="card-list">Zona(s) Horária(s):</span> ${country.timezones}
                 </li>
                 <li>
-                    <a target="_blank" href="${country.maps.googleMaps}">Consultar no Mapa</a>
+                    <a class="text-center text-muted" target="_blank" href="${country.maps.googleMaps}">Consultar no Mapa</a>
                 </li>
             </ul>
         
